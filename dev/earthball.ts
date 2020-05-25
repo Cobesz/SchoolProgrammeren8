fe7387ee374352bdc9b788ede3d9e60358135072
Cas <cas@vorteq.nl>
2020-05-25 14:17:38 +0200

    removed old assignment files

--- dev/earthball.ts
+++
@@ -1,7 +0,0 @@
-class EarthBall extends Ball{
-    constructor(minWidth : number, maxWidth : number) {
-        super(minWidth, maxWidth)
-    }
-}
-
-window.customElements.define("earthball-component", EarthBall as any)
--- dev/moonball.ts
+++
@@ -1,7 +0,0 @@
-class MoonBall extends Ball {
-    constructor(minWidth : number, maxWidth : number) {
-        super(minWidth, maxWidth)
-    }
-}
-
-window.customElements.define("moonball-component", MoonBall as any)
